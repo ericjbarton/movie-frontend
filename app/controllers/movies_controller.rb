73fe6368ec40cf599334d4ee16d0f1470557179b
ericjbarton <erockbart@gmail.com>
2022-08-02 07:33:45 -0400

    adds CRUD actions to movies controller

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,2 +1,44 @@
 class MoviesController < ApplicationController
 end
+
+def index
+  movies = Movie.all.order(:id)
+  render json: movies
+end
+
+def create
+  movie = Movie.new(
+    user_id: current_user.id,
+    title: params[:title],
+    year: params[:year],
+    director: params[:director],
+  )
+  if movie.save
+    render json: movie
+  else
+    render json: { errors: movie.errors.full_messages }, status: :bad_request
+  end
+end
+
+def show
+  movie = Movie.find_by(id: params[:id])
+  render json: movie
+end
+
+def update
+  movie = Movie.find_by(id: params[:id])
+  movie.title = params[:title] || movie.title
+  movie.year = params[:year] || movie.year
+  movie.director = params[:director] || movie.director
+  if movie.save
+    render json: movie
+  else
+    render json: { errors: movie.errors.full_messages }, status: :bad_request
+  end
+
+  def destroy
+    movie = Movie.find_by(id: params[:id])
+    movie.destroy
+    render json: { message: "movie successfully destroyed!" }
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,5 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+movies = Movie.create([title: "Toy Story", year: "1994", director: "John Lassiter"])
+
+movies = Movie.create([title: "Toy Story", year: "1994", director: "John Lassiter"])
+
+movies = Movie.create([title: "Test Story", year: "2022", director: "Test Test is this thing on"])
